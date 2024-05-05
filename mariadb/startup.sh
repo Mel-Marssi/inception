5b2ce1e27fb1edfc071d9e7700d750b2b6ec77f4
mel-Marssi <mel-mars@student.1337.ma>
2024-05-05 16:21:02 +0100

    compose

--- mariadb/startup.sh
+++ mariadb/startup.sh
@@ -1,10 +1,8 @@
-
 service mariadb start
 
-mysql -u root -e "CREATE DATABASE IF NOT EXISTS melmars;"
-mysql -u root -e "CREATE USER IF NOT EXISTS 'Mourad'@'localhost' IDENTIFIED BY '1234';"
-mysql -u root -e "GRANT ALL PRIVILEGES ON melmars.* TO 'Mourad'@'localhost';"
-mysql -u root -e "FLUSH PRIVILEGES;"
-
+mysql -e "CREATE DATABASE IF NOT EXISTS melmars;"
+mysql -e "CREATE USER IF NOT EXISTS 'Mourad'@'%' IDENTIFIED BY '1234';"
+mysql -e "GRANT ALL PRIVILEGES ON melmars.* TO 'Mourad'@'%' IDENTIFIED BY '1234';"
+mysql -e "FLUSH PRIVILEGES;"
 service mariadb stop
-service mariadb start
+exec mysqld_safe
