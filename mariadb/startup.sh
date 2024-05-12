96736251d6a63ed788b80db396b8d47cbc1b05fa
mel-Marssi <mel-mars@student.1337.ma>
2024-05-12 07:58:35 +0100

    mariadb issues

--- mariadb/startup.sh
+++ mariadb/startup.sh
@@ -1,8 +1,9 @@
 service mariadb start
-
+# sed 's'
 mysql -e "CREATE DATABASE IF NOT EXISTS melmars;"
 mysql -e "CREATE USER IF NOT EXISTS 'Mourad'@'%' IDENTIFIED BY '1234';"
 mysql -e "GRANT ALL PRIVILEGES ON melmars.* TO 'Mourad'@'%' IDENTIFIED BY '1234';"
 mysql -e "FLUSH PRIVILEGES;"
 service mariadb stop
-exec mysqld_safe
+service mariadb start
+# exec mysqld_safe
