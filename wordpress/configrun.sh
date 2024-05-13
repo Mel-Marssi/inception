f4614cb629830de0588694bcfc91d184498bc82a
mel-Marssi <mel-mars@student.1337.ma>
2024-05-13 10:43:22 +0100

    config

---
+++ wordpress/configrun.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+cd /var/www/html
+wp core download --allow-root
+wp config create --dbhost=mariadb  --dbname=melmars --dbuser=Mourad --dbpass=1234 --allow-root
+wp core install --url=localhost --title="MyWordPress" --admin_user=mel-mars --admin_password=1234 --admin_email=mel-mars@student.1337.ma --allow-root
+wp user create user1 mourad7elmarssi@gmail.com --user_pass=1234 --allow-root
+exec php-fpm7.4 -F
